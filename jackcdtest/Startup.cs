3b167621b2cc9e52879597f3dff4e056eacc8875
Jack Zeng <v-dazen@microsoft.com>
2017-03-24 14:55:49 +0800

    init

---
+++ jackcdtest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jackcdtest.Startup))]
+namespace jackcdtest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
